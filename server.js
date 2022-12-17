9062611978a7789fac3be1874279f888caaf492f
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2022-12-17 17:00:51 +0100

    ok

--- server.js
+++ server.js
@@ -27,14 +27,7 @@ mongoose
   .catch((err) => console.log("error connecting to mongodb", err));
 
 const PORT = process.env.PORT || 8000;
-if(process.env.NODE_ENV === 'production')
-{
-     app.use('/' , express.static('frontend/build'))
 
-     app.get('*' , (req, res)=>{
-         res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'))
-     })
-}
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}..`);
 });
